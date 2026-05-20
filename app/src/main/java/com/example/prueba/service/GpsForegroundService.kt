32e1e5bace7504753ac775f3c063ab7daf7e6e75
Santi <sgomezp101@gmail.com>
2026-05-20 09:57:50 +0200

    PermissionManager working, next GPS foreground

---
+++ app/src/main/java/com/example/prueba/service/GpsForegroundService.kt
@@ -0,0 +1,13 @@
+//import android.app.Service
+//
+//class GpsForegroundService : Service() {
+//
+//    companion object {
+//        const val CHANNEL_ID = "gps_foreground_channel"
+//        const val NOTIFICATION_ID = 1001
+//        
+//        const val ACTION_START = "ACTION_START_GPS"
+//        const val ACTION_STOP = "ACTION_STOP_GPS"
+//    }
+//}
+
---
+++ app/src/main/java/com/example/prueba/service/ServiceStarter.kt
@@ -0,0 +1,14 @@
+//package com.example.prueba
+//
+////import GpsForegroundService
+//import android.content.Context
+//import android.content.Intent
+//import androidx.core.content.ContextCompat
+//
+//
+//object ServiceStarter {
+//
+//    fun start(context: Context) {
+//        //val intent = Intent(context, GpsForegroundService::class.java)
+//    }
+//}
